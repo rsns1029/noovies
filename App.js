863b27cee12c7fa2ab0a4a83eb91e304c395e4bc
hmchung2 <hmchung2@gmail.com>
2022-07-14 01:43:27 +0900

    Reset

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Text, View, Image } from "react-native";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
+
+export default function App() {
+  const [ready, setReady] = useState(false);
+
+  // --------------Instead of writing all loading assets------------------
+  // const [assets2] = useAssets([require("./splash2.png")]);
+  // const [loaded] = Font.useFonts(Ionicons.font);
+  // if (!assets2 || !loaded) {
+  //   return <AppLoading />;
+  // }
+  // ---------------------------------------------------------------------
+  const loadFonts = (fonts) => {
+    return fonts.map((font) => Font.loadAsync(font));
+  };
+
+  const loadAssets = (images) =>
+    images.map((asset) => {
+      if (typeof asset === "string") {
+        return Image.prefetch(asset);
+      } else {
+        return Asset.loadAsync(asset);
+      }
+    });
+
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadAssets([
+      require("./splash2.png"),
+      "https://images.velog.io/images/jha0402/post/f0fb03e2-852d-4a05-8e7a-a6c7195504aa/react.jpeg",
+    ]);
+    // await Font.loadAsync(Ionicons.font);
+    // await Asset.loadAsync(require("./splash2.png"));
+    // await Image.prefetch("https://url")
+
+    await Promise.all([...fonts, ...images]);
+  };
+
+  const onFinish = () => {
+    console.log("finished");
+    setReady(true);
+  };
+
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+
+  return <Text>We are done loading</Text>;
+}
