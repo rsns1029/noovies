6f7e597bddf0dbedbb1efcede662f8ec8c704849
hmchung2 <hmchung2@gmail.com>
2022-07-15 01:42:09 +0900

    commit

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,12 +3,30 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movie";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
-
+import { Text, View, Image } from "react-native";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
+  <Tab.Navigator
+    initialRouteName="Movies"
+    screenOptions={{
+      tabBarActiveTintColor: "red",
+      tabBarInactiveTintColor: "purple",
+      tabBarStyle: { backgroundColor: "tomato" },
+    }}
+  >
+    <Tab.Screen
+      name="Movies"
+      component={Movies}
+      options={{
+        headerTitleStyle: { color: "tomato" },
+        headerRight: () => (
+          <View>
+            <Text>hello</Text>
+          </View>
+        ),
+      }}
+    />
     <Tab.Screen name="Tv" component={Tv} />
     <Tab.Screen name="Search" component={Search} />
   </Tab.Navigator>
