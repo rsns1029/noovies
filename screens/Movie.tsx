83041f261510e25e717a354ede6d2865253d59e2
hmchung2 <hmchung2@gmail.com>
2022-09-05 00:00:46 +0900

    commit

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,9 +1,11 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import react from "react";
-import { TouchableOpacity } from "react-native";
+import react, { useEffect, useState } from "react";
+import { ActivityIndicator, StyleSheet, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
 import { Dimensions } from "react-native";
+import { makeImgPath } from "../utils";
+import { BlurView } from "expo-blur";
 
 const API_KEY = "ed54fa012e788fb5cefec31666ae71ef";
 
@@ -20,7 +22,8 @@ const API_KEY = "ed54fa012e788fb5cefec31666ae71ef";
 // `;
 
 const Container = styled.ScrollView`
-  background-color: ${(props) => props.theme.mainBgColor};
+  /* background-color: ${(props) =>
+    props.theme.mainBgColor};  already configured at Tab.js*/
 `;
 //ed54fa012e788fb5cefec31666ae71ef
 
@@ -28,17 +31,44 @@ const View = styled.View`
   flex: 1;
 `;
 
+const BgImg = styled.Image``;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  /* background-color: ${(props) => props.theme.mainBgColor}; */
+`;
+
+const Title = styled.Text``;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
   navigation: { navigate },
 }) => {
-  const getNowPlaying = () => {
-    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR
-    `);
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
   };
 
-  return (
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+
+  return loading ? (
+    <Loader>
+      <ActivityIndicator size="large" />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
         loop
@@ -46,10 +76,19 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
         controlsEnabled={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
+        {nowPlaying.map((movie) => {
+          return (
+            <View key={movie.id}>
+              <BgImg
+                style={StyleSheet.absoluteFill}
+                source={{ uri: makeImgPath(movie.backdrop_path) }}
+              />
+              <BlurView intensity={80} style={StyleSheet.absoluteFill}>
+                <Title>{movie.original_title}</Title>
+              </BlurView>
+            </View>
+          );
+        })}
       </Swiper>
     </Container>
   );
